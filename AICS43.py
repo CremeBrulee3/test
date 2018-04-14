7b032513a10a44c544cb7e9ffaac70a4319ab7e1
CremeBrulee3 <CremeBrulee3@users.noreply.github.com>
2018-04-14 14:31:36 -0700

    Add files via upload

---
+++ AICS43.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import csv
+import pandas as pd
+import os
+
+
+data = pd.read_csv('C:\\Users\\Winnie\\Dropbox\\For Next Back Up\\AICS43 GFP VALUES.csv')
+print(data)
+
+
+
+
+"""
+#testing
+print("something")
+print(FITC_list)
+"""
+
